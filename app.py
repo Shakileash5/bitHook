42814801d181eb3c1b65885f32f16edca7014b1b
shakileash <shakileash2000@gmail.com>
2020-12-29 00:38:08 +0530

    json prepared

--- app.py
+++ app.py
@@ -1,20 +1,21 @@
 import requests
 import json
-from flask import Flask,render_template,Response
+from flask import Flask,render_template,Response,jsonify
 import time
 
 key = "9924d3911cf21a14cac79595f1a1b33e"
-url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
 url = "https://api.nomics.com/v1/currencies/ticker?key="+key+"&interval=1d&convert=INR&per-page=100&page=1"
-#res = requests.get(url)
+res = requests.get(url)
 #print(json.loads(res.text))
 
 app = Flask(__name__)
+
 def get_message():
     '''this could be any function that blocks until data is ready'''
-    time.sleep(1.0)
+    time.sleep(20.0)
     s = time.ctime(time.time())
-    return s
+    js = res.json()
+    return json.dumps(js)
 
 @app.route('/')
 def root():
@@ -29,4 +30,4 @@ def stream():
     return Response(eventStream(), mimetype="text/event-stream")
 
 if __name__ == "__main__":
-    app.run()    
+    app.run(port=5000,debug=True)    
