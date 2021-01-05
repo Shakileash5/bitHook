e7123207500df7eebf4daf0f90721bf52622214e
shakileash <shakileash2000@gmail.com>
2021-01-05 15:16:46 +0530

    intermediate commit

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import json
 from flask import Flask,render_template,Response,jsonify,request
 from flask_cors import CORS
 import time
+from datetime import datetime
+import pyrebase
 
 key = "9924d3911cf21a14cac79595f1a1b33e"
 url = "https://api.nomics.com/v1/currencies/ticker" #?key="+key+"&interval=1h,1d&convert=INR&per-page=100&page=1"
@@ -13,6 +15,16 @@ params = {
     "per-page" : 100,
     "page" : 1,
 }
+config = {
+  "apiKey": "AIzaSyBREDzu1TkZWu3L1LBnIJnanDIUsSKlvTs",
+  "authDomain": "bithook-default-rtdb.firebaseio.com/",
+  "databaseURL": "https://bithook-default-rtdb.firebaseio.com/",
+  "projectId": "bithook",
+  "storageBucket": "https://bithook.appspot.com/",
+}
+firebase = pyrebase.initialize_app(config)
+db = firebase.database()
+
 res = requests.get(url,params)
 cryptoCoins = []
 
@@ -20,6 +32,10 @@ cryptoCoins = []
 app = Flask(__name__)
 CORS(app)
 
+
+def saveInFirebase(id,data):
+    db.child("CoinData").child(str(id)).set(data)
+    return
 
 def setHook(data):
     flag = 0
@@ -31,7 +47,11 @@ def setHook(data):
                 del cryptoCoins[i]
     
     if flag == 0:
-        cryptoCoins.append({"id":data["coinId"]})
+        paramsCoin = params
+        paramsCoin["ids"] = str(data["coinId"])
+        res = requests.get(url,paramsCoin).json()
+        print(res[0]["price"],str(datetime.now()),"loaded")
+        cryptoCoins.append({"id":data["coinId"],"hookPrice":res[0]["price"],"hookDateTime":str(datetime.now()),"maxPrice":res[0]["price"]})
 
     params["ids"] = ""
     for coin in cryptoCoins:
@@ -39,7 +59,9 @@ def setHook(data):
                params["ids"] = coin["id"]
            else:
                params["ids"] += "," + coin["id"]
-    print(params,cryptoCoins)         
+    print(params,cryptoCoins)   
+    saveInFirebase(data["userId"],cryptoCoins)    
+    return  
     #response = requests.get(url,params)
     #print("\n\n response\n",response.json()) 
 
